5f2163cf1b6c0d4ec3b912d2d0cad043f707ac8a
Nika <arebanikko@gmail.com>
2025-05-22 20:53:57 +0400

    url encoder

--- src/app/repository/[owner]/[repository]/page.tsx
+++ src/app/repository/[owner]/[repository]/page.tsx
@@ -13,7 +13,7 @@ const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 export default async function Home({ params }: Props) {
   const { owner, repository } = await params;
 
-  const response = await fetch(`https://api.github.com/repos/${owner}/${repository}`, {
+  const response = await fetch(`https://api.github.com/repos/${encodeURIComponent(owner)}/${encodeURIComponent(repository)}`, {
     headers: {
       Authorization: `token ${GITHUB_TOKEN}`,
       Accept: "application/vnd.github+json",
